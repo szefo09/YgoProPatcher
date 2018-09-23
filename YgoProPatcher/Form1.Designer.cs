298ebcef181687f51a84576866a7f683dfdaa292
Przemysław Adamczyk <przemyslaw.adamczyk@microsoft.wsei.edu.pl>
2018-09-23 21:48:05 +0200

    Added Icon

--- YgoProPatcher/Form1.Designer.cs
+++ YgoProPatcher/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -141,7 +142,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
+            this.Opacity = 0.8D;
             this.Text = "YgoPro1 To YgoPro2 Updater";
             this.ResumeLayout(false);
             this.PerformLayout();
