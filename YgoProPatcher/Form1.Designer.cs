1f7bed61ec2c736eef1a3092408bbf5a8a340f1e
Przemysław Adamczyk <przemyslaw.adamczyk@microsoft.wsei.edu.pl>
2018-09-23 19:25:28 +0200

    YgoProPatcher for YGOPRO2

--- YgoProPatcher/Form1.Designer.cs
+++ YgoProPatcher/Form1.Designer.cs
@@ -28,13 +28,137 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.YgoProLinksPath = new System.Windows.Forms.TextBox();
+            this.YgoPro2Path = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(525, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select Path";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.YgoProLinksButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(525, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Select Path";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.YGOPRO2Button_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(141, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Path to YgoProLinks:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Path to YGOPRO2:";
+            // 
+            // YgoProLinksPath
+            // 
+            this.YgoProLinksPath.Location = new System.Drawing.Point(159, 58);
+            this.YgoProLinksPath.Name = "YgoProLinksPath";
+            this.YgoProLinksPath.Size = new System.Drawing.Size(360, 20);
+            this.YgoProLinksPath.TabIndex = 4;
+            // 
+            // YgoPro2Path
+            // 
+            this.YgoPro2Path.Location = new System.Drawing.Point(159, 112);
+            this.YgoPro2Path.Name = "YgoPro2Path";
+            this.YgoPro2Path.Size = new System.Drawing.Size(360, 20);
+            this.YgoPro2Path.TabIndex = 5;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(286, 250);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(182, 82);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Path YGOPRO2 USING YgoProLinks";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.UpdateButton_Click);
+            // 
+            // Status
+            // 
+            this.Status.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Status.Location = new System.Drawing.Point(256, 344);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(246, 23);
+            this.Status.TabIndex = 8;
+            this.Status.Text = "Ready";
+            this.Status.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(286, 381);
+            this.progressBar.Maximum = 90;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(182, 23);
+            this.progressBar.TabIndex = 9;
+            this.progressBar.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.YgoPro2Path);
+            this.Controls.Add(this.YgoProLinksPath);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "YgoPro1 To YgoPro2 Updater";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox YgoProLinksPath;
+        private System.Windows.Forms.TextBox YgoPro2Path;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label Status;
+        private System.Windows.Forms.ProgressBar progressBar;
     }
 }
 
--- YgoProPatcher/Form1.cs
+++ YgoProPatcher/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace YgoProPatcher
 {
@@ -15,6 +16,146 @@ namespace YgoProPatcher
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void YgoProLinksButton_Click(object sender, EventArgs e)
+        {
+            FolderSelection("YGOPro Links");
+        }
+
+        private void YGOPRO2Button_Click(object sender, EventArgs e)
+        {
+            FolderSelection("YGOPRO2");
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            progressBar.Visible = true;
+            Copy("cdb");
+           
+            Copy("script");
+           
+            Copy("pic");
+           
+            Status.Text = "Done!";
+
+        }
+        private void Copy(string type)
+        {
+            string sourcePath = YgoProLinksPath.Text;
+            string targetPath = YgoPro2Path.Text;
+            string filePathYgoPro1="";
+            string filePathYgoPro2="";
+            string fileName;
+            string destFile;
+            
+            if (type == "cdb")
+            {
+                filePathYgoPro1 = @"expansions\live2017links";
+                filePathYgoPro2 = type;
+            }
+            if (type == "script")
+            {
+                filePathYgoPro1 = @"expansions\live2017links\"+type;
+                filePathYgoPro2 = type;
+            }
+            if (type == "pic")
+            {
+                filePathYgoPro1 = type+"s";
+                filePathYgoPro2 = @"picture\card";
+            }
+            try
+            {
+                string fileSource = System.IO.Path.Combine(sourcePath, filePathYgoPro1);
+                string fileDestination = Path.Combine(targetPath, filePathYgoPro2);
+                Status.Text = "Copying "+type+"s";
+                progressBar.Value = 0;
+                if (Directory.Exists(fileSource) && Directory.Exists(fileDestination))
+                {
+                    string partialName = "";
+                    if (type == "cdb")
+                    {
+                        partialName = "prere*";
+                    }
+                    if (type == "pic")
+                    {
+                        partialName = "*.jpg";
+                    }
+                    if (type == "script")
+                    {
+                        partialName = "*.lua";
+                    }
+                    string[] files = Directory.GetFiles(fileSource, partialName);
+                    progressBar.Maximum = files.Length;
+                    foreach (string s in files)
+                    {
+                        if (type == "pic")
+                        {
+                            fileName = Path.ChangeExtension(Path.GetFileName(s), ".png");
+                        }
+                        else
+                        {
+                            fileName = Path.GetFileName(s);
+                        }
+                        destFile = System.IO.Path.Combine(fileDestination, fileName);
+                        if (type == "pic")
+                        {
+                            if (!File.Exists(destFile))
+                            {
+                                System.IO.File.Copy(s, destFile, false);
+                            }
+                        }
+                        else
+                        {
+                            System.IO.File.Copy(s, destFile, true);
+                        }
+                        
+                        
+                        progressBar.Increment(1);
+                    }
+                    if (type == "cdb")
+                    {
+                        fileName = "official.cdb";
+                        destFile = System.IO.Path.Combine(fileDestination, fileName);
+                        System.IO.File.Copy(Path.Combine(fileSource, fileName), destFile, true);
+                        fileName = "cards.cdb";
+                        fileDestination = Path.Combine(targetPath, type+@"\English");
+                        destFile = System.IO.Path.Combine(fileDestination, fileName);
+                        System.IO.File.Copy(Path.Combine(sourcePath, fileName), destFile, true);
+                    }
+
+
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Unexpected error occured, please make sure you've put correct file paths");
+            }
+
+        }
+        private string FolderSelection(string versionOfYGO)
+        {
+            string folderString = "";
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowNewFolderButton = false;
+            fbd.Description = "Select main folder of " + versionOfYGO;
+            DialogResult result = fbd.ShowDialog();
+            if(result == DialogResult.OK)
+            {
+                if (versionOfYGO == "YGOPRO2")
+                {
+                    YgoPro2Path.Text = fbd.SelectedPath;
+                }
+                else
+                {
+                    YgoProLinksPath.Text = fbd.SelectedPath;
+                }
+
+               
+            }
+
+            return folderString;
         }
     }
 }
