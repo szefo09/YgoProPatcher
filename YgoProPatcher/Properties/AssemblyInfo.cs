b1b690273809465ed91070c07b732f39448e23a3
Przemysław Adamczyk <przemyslaw.adamczyk@microsoft.wsei.edu.pl>
2018-09-27 23:06:01 +0200

    some assembly changes

--- YgoProPatcher/Properties/AssemblyInfo.cs
+++ YgoProPatcher/Properties/AssemblyInfo.cs
@@ -6,10 +6,10 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YgoProPatcher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Update Your YGOPRO2 Now!")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("YgoProPatcher")]
+[assembly: AssemblyProduct("Update Your YGOPRO2 Now!")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.45.0")]
