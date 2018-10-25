acf174949231f2e56bd67ec6e6016a93ffe57081
Przemysław Adamczyk <przemyslaw.adamczyk@microsoft.wsei.edu.pl>
2018-10-26 00:55:55 +0200

    fixed a label's text

--- YgoProPatcher/Form1.Designer.cs
+++ YgoProPatcher/Form1.Designer.cs
@@ -212,9 +212,9 @@
             this.OverwriteCheckbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.OverwriteCheckbox.Location = new System.Drawing.Point(12, 192);
             this.OverwriteCheckbox.Name = "OverwriteCheckbox";
-            this.OverwriteCheckbox.Size = new System.Drawing.Size(149, 38);
+            this.OverwriteCheckbox.Size = new System.Drawing.Size(153, 38);
             this.OverwriteCheckbox.TabIndex = 16;
-            this.OverwriteCheckbox.Text = "Overwrite Pics\r\n(Not reccomended)";
+            this.OverwriteCheckbox.Text = "Overwrite Pics\r\n(Not recommended)";
             this.OverwriteCheckbox.UseVisualStyleBackColor = true;
             // 
             // debug
